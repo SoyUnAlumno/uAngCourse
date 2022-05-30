5d5c08dd4c2c640dfc7154f00a9757b6645a266c
SoyUnAlumno <tomaspraga@hotmail.es>
2022-05-30 11:04:23 +0200

    small test

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './mods-home.component.html',
   styleUrls: ['./mods-home.component.css']
 })
+
+
 export class ModsHomeComponent implements OnInit {
 modalOpen = false;
   constructor() { }
